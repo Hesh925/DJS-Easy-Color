ae60b39abdb7bfc42732a0c822fa908b44eabfff
Tyler <Tyler@heshmati.com>
2021-05-28 18:58:12 -0700

    Fixed getRandomGenTest,js

--- test/tests/getRandomGenTest.js
+++ test/tests/getRandomGenTest.js
@@ -1,7 +1,7 @@
-const color = require('../src/main.js')
+const color = require('../../src/main.js')
 const times = 100
 
 let i
 for (i = 0; i < times; i++) {
-    console.log(`${i}, HEX: ${color.getRandomgen("HEX")}`)
+    console.log(`${i}, HEX: ${color.getRandomGen("HEX")}`)
 }
